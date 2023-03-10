fe32c85d159a312454472292a6ab60a7a5501365
tonyandy5630 <tonyandy456@gmail.com>
2023-03-10 19:02:08 +0700

    add sendMail function

---
+++ utils/mail.js
@@ -0,0 +1,31 @@
+const sgMail = require("@sendgrid/mail");
+const dotenv = require("dotenv");
+const { MAIL_SENDER } = require("../constants/auth");
+
+dotenv.config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.sendMail = ({ sendTo, subject, text, html }) => {
+  const mail = {
+    to: sendTo,
+    from: MAIL_SENDER,
+    subject,
+    text,
+    html,
+  };
+
+  sgMail.send(mail).then(
+    () => {
+      console.log(sendTo);
+      console.log("sent");
+    },
+    (err) => {
+      console.error(err);
+
+      if (err.response) {
+        console.error(err.response.body);
+      }
+    }
+  );
+};
