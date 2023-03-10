e70c35b38478c06d0b0bdf399e6ac639e942e841
tonyandy5630 <tonyandy456@gmail.com>
2023-03-10 19:02:25 +0700

    add signup controller

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,30 @@
 const express = require("express");
 
 const router = express.Router();
-
+const { body } = require("express-validator");
 const User = require("../model/user");
 const authController = require("../controllers/auth");
 
 router.post("/login", authController.login);
+
+router.post(
+  "/signup",
+  [
+    body("email")
+      .isEmail()
+      .withMessage("Enter an email")
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((user) => {
+          if (user) {
+            return Promise.reject("Email already exist");
+          }
+        });
+      })
+      .normalizeEmail(),
+    body("pwd").trim().isLength({ min: 5 }),
+    body("fullname").trim().not().isEmpty(),
+  ],
+  authController.signUp
+);
 
 module.exports = router;
