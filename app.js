de37c2503de3ddc323688ffdaac1167fd228f141
tonyandy5630 <tonyandy456@gmail.com>
2023-03-15 18:07:34 +0700

    add session

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ const app = express();
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const { MONGODB_URI } = require("./constants/shared");
+const passport = require("passport");
+const session = require("express-session");
+const { MONGODB_URI, PORT } = require("./constants/shared");
+require("./utils/passport");
 //* Routes
 const authRoutes = require("./routes/auth");
 
 app.use(helmet());
 app.use(morgan("combined"));
+app.use(
+  session({
+    secret: "SECRET",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -21,14 +37,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
-
 app.use("/auth", authRoutes);
 
 mongoose
   .connect(MONGODB_URI)
   .then((res) => {
-    console.log("connect success");
-    app.listen(8080);
+    console.log("connect success at " + PORT);
+    app.listen(PORT);
   })
   .catch((err) => console.log(err));
