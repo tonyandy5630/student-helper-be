415178d5c42d81b2074c90244272cfce197b1c8a
tonyandy5630 <tonyandy456@gmail.com>
2023-03-15 18:07:45 +0700

    change constants

--- constants/auth.js
+++ constants/auth.js
@@ -4,4 +4,7 @@ dotenv.config();
 module.exports = {
   SECRET_KEY: process.env.SECRET_KEY,
   MAIL_SENDER: "tonyandy456@protonmail.com",
+  OAUTH_ID: process.env.OATH_ID,
+  OAUTH_REDIRECT_URL: process.env.OAUTH_REDIRECT_URL,
+  OATH_CLIENT_SECRET: process.env.OAUTH_CLIENT_SECRET,
 };
--- constants/shared.js
+++ constants/shared.js
@@ -1,6 +1,9 @@
 const dotenv = require("dotenv");
-dotenv.config();
+dotenv.config({ override: true });
 
 module.exports = {
   MONGODB_URI: process.env.MONGODB_URI,
+  APP_NAME: "RESME",
+  PORT: process.env.SERVER_PORT,
+  CLIENT_URL: process.env.CLIENT_URL,
 };
